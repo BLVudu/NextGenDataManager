2ef2156e7fe4f06ca9fdd06589f37efb0b1981fe
plin2 <ping-hsien.lin@vuud.com>
2016-10-24 11:54:14 -0700

    Initial Commit

---
+++ NextGenDataManager/NextGenDataManager.h
@@ -0,0 +1,19 @@
+//
+//  NextGenDataManager.h
+//  NextGenDataManager
+//
+//  Created by Pinghsien Lin on 10/24/16.
+//  Copyright © 2016 vudu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NextGenDataManager.
+FOUNDATION_EXPORT double NextGenDataManagerVersionNumber;
+
+//! Project version string for NextGenDataManager.
+FOUNDATION_EXPORT const unsigned char NextGenDataManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NextGenDataManager/PublicHeader.h>
+
+
