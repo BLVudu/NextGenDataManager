f7530deccd30227ef798546b5b8a7531cbbc491b
plin2 <ping-hsien.lin@vuud.com>
2017-04-26 16:45:59 -0700

    xml

---
+++ Source/Network/Network Models/ProductItem.swift
@@ -0,0 +1,38 @@
+//
+//  ProductItem.swift
+//
+
+import Foundation
+
+@objc public protocol ProductItem {
+
+    var id: String { get }
+    var externalURL: URL? { get }
+    var name: String { get }
+    var brand: String? { get }
+    @objc optional var category: ProductCategory? { get }
+    var displayPrice: String? { get }
+    var productImageURL: URL? { get }
+    var sceneImageURL: URL? { get }
+    var isExactMatch: Bool { get }
+    var hasExactMatchData: Bool { get }
+    var bullseyePoint: CGPoint { get }
+    var shareText: String { get }
+
+}
+
+public func == (lhs: ProductItem, rhs: ProductItem) -> Bool {
+    return lhs.id == rhs.id
+}
+
+public func != (lhs: ProductItem, rhs: ProductItem) -> Bool {
+    return lhs.id != rhs.id
+}
+
+public func == (lhs: ProductItem?, rhs: ProductItem?) -> Bool {
+    return lhs?.id == rhs?.id
+}
+
+public func != (lhs: ProductItem?, rhs: ProductItem?) -> Bool {
+    return lhs?.id != rhs?.id
+}
